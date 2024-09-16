0d10e60108a49ce05d659d3603a7e3d668026428
ashu8983 <166315512+ashu8983@users.noreply.github.com>
2024-09-16 23:12:01 +0530

    Add files via upload

---
+++ fibonacci_generator.py
@@ -0,0 +1,13 @@
+
+def fibo(n):
+    if n==1:
+        return 0
+    if n==2:
+        return 1
+    return (fibo(n-2) + fibo(n-1))
+
+
+n=int(input("Enter number of terms: "))
+for i in range(1,n+1):
+    print(fibo(i))
+
